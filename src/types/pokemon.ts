768dc9121f7230b855bdad2d3e7109964d4daf79
Enyel Liranzo <xardimodschannel@gmail.com>
2024-03-11 11:53:13 -0400

    fix: making changes in interfaces

--- src/types/pokemon.ts
+++ src/types/pokemon.ts
@@ -102,7 +102,18 @@ export type Sprites = {
   front_shiny_female: string;
   other?: Other;
   versions?: Versions;
-  animated?: Sprites;
+  animated?: Animated;
+}
+
+export type Animated = {
+  back_default: string;
+  back_female: string;
+  back_shiny: string;
+  back_shiny_female: string;
+  front_default: string;
+  front_female: string;
+  front_shiny: string;
+  front_shiny_female: string;
 }
 
 export type GenerationI = {
